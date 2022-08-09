d82784507cf3fe22e3ff8a2806e0a8d9fbc60c95
PChemGuy <39730837+pchemguy@users.noreply.github.com>
2022-08-09 17:32:01 +0300

    highlight

---
+++ assets/js/custom.js
@@ -0,0 +1,5 @@
+//<link rel="stylesheet" href="/path/to/styles/default.min.css">
+//<script src="/path/to/highlight.min.js"></script>
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/github.min.css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/highlight.min.js"></script>
+<script>hljs.highlightAll();</script>
